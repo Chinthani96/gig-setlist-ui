f945ae154dc72e45f337b00c73d428bd5004efc4
Ashan Kaushalya <ashankaushalya952@gmail.com>
2022-10-30 08:01:10 +0530

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,33 +1,44 @@
-import { AppBar, Button, IconButton, makeStyles, Theme, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor:"#567567"
-      },
-      menuButton: {
-        marginRight: theme.spacing(2),
-      },
-      title: {
-        flexGrow: 1,
-      },
-}))
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#567567",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const GSAppBar = () => {
-    const classes = useStyles();
-    return(
-        <AppBar position="static" className={classes.root}>
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-     
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      Gig Setlist
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
-    )
-}
+  const classes = useStyles();
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        ></IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Gig Setlist
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default GSAppBar;
